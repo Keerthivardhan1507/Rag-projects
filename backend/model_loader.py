ff06fcf22c13dfddea7ba4fa46d998b1e72fce8f
Keerthivardhan1507 <nettemKeerthivardhannaidu1507@gmail.com>
2026-07-22 16:11:51 +0530

    model_loader:

--- backend/model_loader.py
+++ backend/model_loader.py
@@ -2,27 +2,38 @@ import os
 from backend.config_loader import load_config
 from dotenv import load_dotenv
 from langchain_groq import ChatGroq
-from pydantic import BaseModel
-from typing import Literal,List,Any,Dict
+from pydantic import BaseModel,Field
+from typing import Literal,List,Any,Dict,Optional
 
 class ConfigLoader:
     def __init__(self):
-        pass
+        print("loadinf Config")
+        self.config = load_config()
     
     def __getitem__(self, key):
-        pass
+        return self.config[key]
     
 class ModelLoader(BaseModel):
-    def __init__(self):
-        pass
+    model_provider :Literal["groq"] = "groq"
+    config :Optional[ConfigLoader] = Field(default=None,exclude=True)
     
-    def model_post_init():
-        pass
-    
+    def model_post_init(self,__context:Any)->None:
+        self.config = ConfigLoader()    
     class Config:
+        arbitrary_types_allowed = True
         
-        def load_llm(self):
-            pass
+    def load_llm(self):
+        """Load the llm model
+        """
+        print(f"loading model")
+        print(f"Loading llm model {self.model_provider}")
+        
+        if self.model_provider == "groq":
+            print("loading groq model")
+            groq_api_key = os.environ["GROQ_API_KEY"]
+            model_name = self.config["llm"]["groq"]
+            llm = ChatGroq(model=model_name,api_key=groq_api_key)
+        return llm
             
 
 
